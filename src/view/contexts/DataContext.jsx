7685d1872044aba9e4ea912141cf8d0f89eb4001
pl <plrtn@protonmail.com>
2019-12-10 00:25:25 -0500

    Added fetch functionality to debounce hook , and reduce display render

--- src/view/contexts/DataContext.jsx
+++ src/view/contexts/DataContext.jsx
@@ -1,6 +1,8 @@
 import React, { useState, createContext } from 'react'
 import { useDebounce } from 'react-use'
 import { constraint } from '../../model/InputForm'
+import { postData } from '../../action/API'
+import { prettyPrint } from '../../helper/printer'
 import validate from 'validate.js'
 
 const Context = createContext(undefined)
@@ -9,18 +11,30 @@ const Context = createContext(undefined)
 const useDataContext = () => {
   const [state, setState] = useState({
     input: '',
-    output: '',
     hadFocus: false
   })
-  
+
+  const [display, setDisplay] = useState()
+
   // if state is validation passing, validate.js returns undefined. Eui takes an array of strings as error messages
   const errorMsg  = !validate(state, constraint)? [] : validate(state, constraint).input
   const hasError  = state.hadFocus && validate(state, constraint) !== undefined
 
   // note: useDebounce allows side effects
-  useDebounce(() => {
-    if (!hasError){
-      setState({...state, output: state.input})
+  useDebounce( () => {
+    // This makes sure the display is only set when
+    // 1. The input has been interacted with at least once
+    // 2. It does not have any errors
+    if(state.hadFocus && !hasError){
+      (async () => { 
+        try{
+          const { output }  =  await postData({input: parseInt(state.input)})
+          setDisplay(prettyPrint(state.input, JSON.stringify(output)))
+        } catch (err) {
+          // swallow
+          // I can't think of a way to actually get here, but in case somehow validation is bypassed and I am doing a parseInt on a str
+        }
+      })()
     }
   }, 450 , [state])
 
@@ -28,7 +42,8 @@ const useDataContext = () => {
     state,
     setState,
     errorMsg,
-    hasError
+    hasError,
+    display,
   }
 }
 
