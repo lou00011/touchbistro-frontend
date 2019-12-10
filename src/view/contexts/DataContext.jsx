404b65c9352f6746ab78ed07798cd30572b435f8
pl <plrtn@protonmail.com>
2019-12-09 19:29:28 -0500

    Added debounce functionality

--- src/view/contexts/DataContext.jsx
+++ src/view/contexts/DataContext.jsx
@@ -1,4 +1,5 @@
 import React, { useState, createContext } from 'react'
+import { useDebounce } from 'react-use'
 import { constraint } from '../../model/InputForm'
 import validate from 'validate.js'
 
@@ -8,12 +9,20 @@ const Context = createContext(undefined)
 const useDataContext = () => {
   const [state, setState] = useState({
     input: '',
+    output: '',
     hadFocus: false
   })
   
   // if state is validation passing, validate.js returns undefined. Eui takes an array of strings as error messages
   const errorMsg  = !validate(state, constraint)? [] : validate(state, constraint).input
   const hasError  = state.hadFocus && validate(state, constraint) !== undefined
+
+  // note: useDebounce allows side effects
+  useDebounce(() => {
+    if (!hasError){
+      setState({...state, output: state.input})
+    }
+  }, 450 , [state])
 
   return {
     state,
