b185dd17aa9cb93b0bef49c357789c58faa7d645
pl <plrtn@protonmail.com>
2019-12-10 00:19:28 -0500

    edited post req function, added working tests

---
+++ src/action/API.test.js
@@ -0,0 +1,38 @@
+import {postData} from './API'
+
+describe('API fetch/POST tests', () => {
+  test('Valid - return json contain right properties', async (done) => {
+    const actual = await postData({input: 10})
+    const expected = 'output'
+    expect(actual).toHaveProperty(expected)
+    done()
+  })
+
+  test('Valid - return json return correct calculations', async (done) => {
+    const actual = await postData({input: 10})
+    const expected = {output: [3,5]}
+    expect(actual).toStrictEqual(expected)
+    done()
+  })
+
+  test('Valid - return json return correct calculations #2', async (done) => {
+    const actual = await postData({input: 12})
+    const expected = {output: [5]}
+    expect(actual).toStrictEqual(expected)
+    done()
+  })
+
+  test('Valid - return json return correct calculations #3', async (done) => {
+    const actual = await postData({input: 11})
+    const expected = {output: [3,5]}
+    expect(actual).toStrictEqual(expected)
+    done()
+  })
+
+  test('Valid - return json return correct calculations #4', async (done) => {
+    const actual = await postData({input: 2})
+    const expected = {output: []}
+    expect(actual).toStrictEqual(expected)
+    done()
+  })
+})
--- src/action/API.ts
+++ src/action/API.ts
@@ -1,14 +1,18 @@
-const url = ''
+const url = 'https://touchbistro-backend.herokuapp.com/'
 
-function postData(data: any){
-  return fetch(url, {
-    method: 'POST',
+async function postData(data: any){
+  const _body = JSON.stringify(data)
+  const res = await fetch(url, {
+    method: 'post',
     mode: 'cors',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
     },
-    body: JSON.stringify(data)
-  }).then(x => x.json)
+    body: _body
+  })
+
+  return res.json()
 }
 
 export {
