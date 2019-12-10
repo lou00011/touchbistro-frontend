405beabb929edff581dc72344e4246036b5b8070
pl <plrtn@protonmail.com>
2019-12-10 00:21:01 -0500

    Deleted unused serviceworker file, serviceworker imports, comments and jest setup

--- src/setupTests.js
+++
@@ -1,7 +0,0 @@
-// https://create-react-app.dev/docs/running-tests/
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ 
-  adapter: new Adapter()
-});
